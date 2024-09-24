3b3c65c5c168e9ebc4a0640d21f194dab1cbc090
Fer Marquez <fernanda.marquezgonzlez@gmail.com>
2024-09-24 13:22:04 -0600

    Remove unnecessary console.log call

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("My First API RESTFul with Node js")
+    res.send("My First API RESTFul with Node js!")
 })
 
 app.listen(3000, () =>{
