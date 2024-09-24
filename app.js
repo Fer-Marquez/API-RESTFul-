e83db66e040fb47a4baff6e1b884c5f9d91484dc
Fer Marquez <fernanda.marquezgonzlez@gmail.com>
2024-09-24 13:16:35 -0600

    Init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("My First API RESTFul with Node js")
+})
+
+app.listen(3000, () =>{
+    console.log('Server listening on port 3000')
+});
