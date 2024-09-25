871aafc28b8c13422cc9e9d52fbb9305b388ab35
Fer Marquez <fernanda.marquezgonzlez@gmail.com>
2024-09-25 12:50:27 -0600

    Actualizar

--- app.js
+++ app.js
@@ -4,15 +4,37 @@ import fs from "fs";
 const app = express();
 
 const readData = () => {
+  try {
     const data = fs.readFileSync("./db.json");
-    console.log(data);
+    return JSON.parse(data);
+  } catch (error) {
+        console.log(error);
+  }
 };
 
-readData();
+const writeData = (data) => {
+    try {
+       fs.writeFileSync("./db.json", JSON.stringify(data));
+      } catch (error) {
+        console.log(error);
+      }  
+} 
 
 app.get("/", (req, res) => {
     res.send("My First API RESTFul with Node js!")
-})
+});
+
+app.get("/books", (req, res) => {
+    const data = readData();
+    res.json(data.books);
+});
+
+app.get("/books/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const book = data.books.find((book) => book.id === id);
+    res.json(book);
+  });
 
 app.listen(3000, () =>{
     console.log('Server listening on port 3000!')
