49fef63ca164d2bdd0083c55ea18ccb1f66bf756
Fer Marquez <fernanda.marquezgonzlez@gmail.com>
2024-09-26 12:46:51 -0600

    Actualizar

--- routes/auth.js
+++ routes/auth.js
@@ -1,34 +1,34 @@
 const express = require('express');
-// const { body } = require('express-validator');
+const { body } = require('express-validator');
 
-// const User = require('../models/user');
-// const authController = require('../controllers/auth');
+const User = require('../models/user');
+const authController = require('../controllers/auth');
 
 const router = express.Router();
 
 router.put(
-   '/signup'//,
-//   [
-//     body('email')
-//       .isEmail()
-//       .withMessage('Please enter a valid email.')
-//       .custom((value, { req }) => {
-//         return User.findOne({ email: value }).then(userDoc => {
-//           if (userDoc) {
-//             return Promise.reject('E-Mail address already exists!');
-//           }
-//         });
-//       })
-//       .normalizeEmail(),
-//     body('password')
-//       .trim()
-//       .isLength({ min: 5 }),
-//     body('name')
-//       .trim()
-//       .not()
-//       .isEmpty()
-//   ],
-//   authController.signup
+   '/signup',
+  [
+    body('username')
+      .isUsername()
+      .withMessage('Please enter a valid username.')
+      .custom((value, { req }) => {
+        return User.findOne({ username: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject('Username is already exists!');
+          }
+        });
+      })
+      .normalizeUsername(),
+    body('password')
+      .trim()
+      .isLength({ min: 5 }),
+    body('name')
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  authController.signup
 );
 
 // router.post('/login', authController.login);
