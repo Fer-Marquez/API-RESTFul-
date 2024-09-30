15c1f2665fb2abd1e907d261e656670aeb695e31
Fer Marquez <fernanda.marquezgonzlez@gmail.com>
2024-09-30 11:59:03 -0600

    Actualizar

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,15 +57,15 @@ exports.login = (req, res, next) => {
         error.statusCode = 401;
         throw error;
       }
-      // const token = jwt.sign(
-      //   { 
-      //     email: loadedUser.email,
-      //     userId: loadedUser._id.toString()
-      //   },
-      //   'somesupersecretsecret',
-      //   { expiresIn: '1h' }
-      // );
-      // res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+      const token = jwt.sign(
+        { 
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        'somesupersecretsecret',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch(err => {
       if (!err.statusCode) {
