05310e1257b42f6b916552b0d319bd353baff255
Fer Marquez <fernanda.marquezgonzlez@gmail.com>
2024-10-02 13:03:56 -0600

    Actualizado

--- app.js
+++ app.js
@@ -7,75 +7,8 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 
-app.use(bodyParser.json());
-
-const readData = () => {
-  try {
-    const data = fs.readFileSync("./db.json");
-    return JSON.parse(data);
-  } catch (error) {
-        console.log(error);
-  }
-};
-
-const writeData = (data) => {
-    try {
-       fs.writeFileSync("./db.json", JSON.stringify(data));
-      } catch (error) {
-        console.log(error);
-      }  
-} 
-
-app.get("/", (req, res) => {
-    res.send("My First API RESTFul with Node js!")
-});
-
-app.get("/books", (req, res) => {
-    const data = readData();
-    res.json(data.books);
-});
-
-app.get("/books/:id", (req, res) => {
-    const data = readData();
-    const id = parseInt(req.params.id);
-    const book = data.books.find((book) => book.id === id);
-    res.json(book);
-  });
-
-  app.post("/books", (req, res) => {
-    const data = readData();
-    const body = req.body;
-    const newBook = {
-      id: data.books.length + 1,
-      ...body,
-    };
-    data.books.push(newBook);
-    writeData(data);
-    res.json(newBook);
-  });
-
-  app.put("/books/:id", (req, res) => {
-    const data = readData();
-    const body = req.body;
-    const id = parseInt(req.params.id);
-    const bookIndex = data.books.findIndex((book) => book.id === id);
-    data.books[bookIndex] = {
-      ...data.books[bookIndex],
-      ...body,
-    };
-    writeData(data);
-    res.json({ message: "Book updated successfully" });
-  });
+  app.use(bodyParser.json());
   
-  app.delete("/books/:id", (req, res) => {
-    const data = readData();
-    const id = parseInt(req.params.id);
-    const bookIndex = data.books.findIndex((book) => book.id === id);
-    data.books.splice(bookIndex, 1);
-    writeData(data);
-    res.json({ message: "Book deleted successfully" });
-  });
-
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
--- routes/feed.js
+++ routes/feed.js
@@ -6,6 +6,84 @@ const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
+// const express = require('express');
+const fs = require("fs");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const authRoutes = require('./routes/auth');
+
+// const app = express();
+
+app.use(bodyParser.json());
+
+const readData = () => {
+  try {
+    const data = fs.readFileSync("./db.json");
+    return JSON.parse(data);
+  } catch (error) {
+        console.log(error);
+  }
+};
+
+const writeData = (data) => {
+    try {
+       fs.writeFileSync("./db.json", JSON.stringify(data));
+      } catch (error) {
+        console.log(error);
+      }  
+} 
+
+app.get("/", (req, res) => {
+    res.send("Sistema de Autenticación y CRUD de Usuarios")
+});
+
+app.get("/api", (req, res) => {
+    const data = readData();
+    res.json(data.userId);
+});
+
+app.get("/api/user/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const userId = data.userId.find((user) => user.id === id);
+    res.json(user);
+  });
+
+  app.post("/api/auth/signup", (req, res) => {
+    const data = readData();
+    const user = {
+      userId: userId,
+      email: email
+    };
+    data.userId.push(newUser);
+    writeData(data);
+    res.json(newUser);
+  });
+
+  app.put("/api/user/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const bookIndex = data.books.findIndex((book) => book.id === id);
+    data.books[bookIndex] = {
+      ...data.books[bookIndex],
+      ...body,
+    };
+    writeData(data);
+    res.json({ message: "User updated successfully" });
+  });
+  
+  app.delete("/api/user/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const bookIndex = data.books.findIndex((book) => book.id === id);
+    data.books.splice(bookIndex, 1);
+    writeData(data);
+    res.json({ message: "User deleted successfully" });
+  });
+
+
+
 // // GET /feed/posts
 // router.get('/posts', isAuth, feedController.getPosts);
 
